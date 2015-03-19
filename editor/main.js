397c9f866aca6be1ff0aae17dd5f7da1ebd145c1
Johnny Wu <KarasAya@gmail.com>
2015-03-19 14:09:21 +0800

    update fireball-x/dev#163, allow undock panel

--- editor/main.js
+++ editor/main.js
@@ -7,16 +7,19 @@ module.exports = {
             var ext = Url.extname(url);
 
             if ( ['.js', '.json', '.xml', '.html', '.css','.styl','.htm'].indexOf(ext.toLowerCase()) !== -1 ) {
-                if ( context.openedWindows.length > 0 ) {
-                    var firstWindow = context.openedWindows[0];
-                    firstWindow.sendToPage( 'asset:edit', uuid );
-                    firstWindow.focus();
-                }
-                else {
-                    context.openWindow('code-editor', {
-                        uuid: uuid,
-                    });
-                }
+                // if ( context.openedWindows.length > 0 ) {
+                //     var firstWindow = context.openedWindows[0];
+                //     firstWindow.sendToPage( 'asset:edit', uuid );
+                //     firstWindow.focus();
+                // }
+                // else {
+                //     context.openPanel('code-editor', {
+                //         uuid: uuid,
+                //     });
+                // }
+                context.openPanel('code-editor', {
+                    uuid: uuid,
+                });
             }
         });
     },
